23244b3f13667abeddde3d09b02bb142650b13d8
poorviag22 <85426325+poorviag22@users.noreply.github.com>
2024-11-05 18:49:20 +0530

    Delete Flipfit-JEDI-Java-POS directory

--- Flipfit-JEDI-Java-POS/src/com/flipfit/business/CenterBusiness.java
+++
@@ -1,8 +0,0 @@
-package com.flipfit.business;
-
-public interface CenterBusiness {
-    public String viewSlots(int centerId);
-    public boolean bookSlot(int centerId,int slotId);
-    public boolean cancelBooking(int centerId,int slotId);
-    public int waitlist(int centerId,int slotId);
-}
--- Flipfit-JEDI-Java-POS/src/com/flipfit/business/SlotsBusiness.java
+++
@@ -1,6 +0,0 @@
-package com.flipfit.business;
-
-public interface SlotsBusiness {
-    public boolean isBooked(int slotId);
-
-}
